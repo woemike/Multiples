5aaf726c0b243352dce821136ab54b69f8b104d2
Alexandru Mihai Ion <ionmihai09@gmail.com>
2016-03-14 21:38:22 +0000

    whole app

---
+++ README.md
@@ -0,0 +1,3 @@
+# iOS 9 and Swift 2: From Beginner to Paid Professional
+### Udemy course
+###### Multiples is a basic mobile app made using Swift 2. A user taps a number of time that he/she has set at the beginning of the game in order to see all the multiples of that number until number * 10. Then the game restarts itself!
--- multiples-of-two/ViewController.swift
+++ multiples-of-two/ViewController.swift
@@ -10,16 +10,88 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOutlets
+    @IBOutlet weak var whatMultipleTxt: UITextField!
+    @IBOutlet weak var sumLbl: UILabel!
+    @IBOutlet weak var logoImg: UIImageView!
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var playBtn: UIButton!
+    
+    //Properties
+    var multiple = 0
+    var runningSum = 0
+    let MAX_SUM = 50
+ 
+
+    @IBAction func onAddBtnTapped(sender: AnyObject) {
+        //Since we never change the newSum value make it a constant
+        let newSum = runningSum + multiple
+        updateLbl(runningSum, mul: multiple, newSum: newSum)
+        runningSum += multiple
+        
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    
+    @IBAction func onPlayBtnTapped(sender: AnyObject) {
+        
+        if whatMultipleTxt.text != nil && whatMultipleTxt.text != "" {
+            
+            whatMultipleTxt.hidden = true
+            logoImg.hidden = true
+            playBtn.hidden = true
+            sumLbl.hidden = false
+            addBtn.hidden = false
+            
+            resetLbl()
+            multiple = Int(whatMultipleTxt.text!)!
+            runningSum = 0
+        }
+    }
+    
+    func restartGame() {
+        multiple = 0
+        whatMultipleTxt.text = ""
+        whatMultipleTxt.hidden = false
+        logoImg.hidden = false
+        playBtn.hidden = false
+        sumLbl.hidden = true
+        addBtn.hidden = true
+        resetLbl()
+    }
+    
+    func resetLbl() {
+        sumLbl.text = "Press Add to add!"
+    }
+    
+    func isGameOver() -> Bool {
+        if runningSum >= MAX_SUM {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func updateLbl(oldSum: Int, mul: Int, newSum: Int) {
+        sumLbl.text = "\(oldSum) + \(mul) = \(newSum)"
+    }
+    
+    // Dismiss keyboard section
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
-
+    
 
 }
 
